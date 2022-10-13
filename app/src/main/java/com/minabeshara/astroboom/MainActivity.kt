377e1ef2b898c7305886d1df36b5197ac9d36566
minaNew10 <minadevdream@gmail.com>
2022-10-13 12:25:49 +0200

    add dependencies and main screen

--- app/src/main/java/com/minabeshara/astroboom/MainActivity.kt
+++ app/src/main/java/com/minabeshara/astroboom/MainActivity.kt
@@ -2,10 +2,12 @@ package com.minabeshara.astroboom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.minabeshara.astroboom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this,R.layout.activity_main)
     }
 }
